e6cdfce6a106c976152cb59b1993c14da32f6a7b
AayamPaudel <blox46119@gmail.com>
2026-05-20 19:20:47 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let div = document.getElementById("div")
 
-let firstinput = document.getElementById("first")
-
 let middleinput = document.getElementById("middle")
 
 let lastinput = document.getElementById("last")
@@ -10,6 +8,7 @@ let h1 = document.getElementById("h1")
 
 let p = document.getElementById("name")
 
+let firstinput = document.getElementById("one")
 
 div.style.display = "none"
 
@@ -20,9 +19,14 @@ const form = document.getElementById("form")
 form.addEventListener("submit", function(event){
     event.preventDefault()
 
-
+    first = firstinput.value
+    middle = middleinput.value
+    last = lastinput.value
     h1.style.dislay = "block"
     div.style.display = "block"
     form.style.display = "none"
-    p.innerText = "" + first.value + " " + middle.value + " " + last.value
+    p.innerText = "" + first + " " + middle + " " + last
+    firstinput.value = ""
+    middleinput.value = ""
+    lastinput.value = ""
 })
